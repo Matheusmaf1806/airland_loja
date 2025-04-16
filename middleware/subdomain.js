b58694c49a296339c37ae56cbdae16b555820c7a
Matheusmaf1806 <matheus.alvarenga@airland.com.br>
2025-04-15 21:36:06 -0300

    Update subdomain.js

--- middleware/subdomain.js
+++ middleware/subdomain.js
@@ -1,6 +1,13 @@
 // middleware/subdomain.js
-module.exports = function (req, res, next) {
-  let host = req.headers.host;
-  req.subdomain = host ? host.toLowerCase() : null;
+module.exports = (req, res, next) => {
+  const host = req.headers.host; // Por exemplo: lucastur.airland.com.br
+  if (host && host.endsWith('airland.com.br')) {
+    const parts = host.split('.');
+    // Se o host for composto por 4 partes, assumimos que a primeira é o subdomínio:
+    // Ex.: [lucastur, airland, com, br]
+    req.subdomain = parts.length >= 4 ? parts[0] : null;
+  } else {
+    req.subdomain = null;
+  }
   next();
 };
