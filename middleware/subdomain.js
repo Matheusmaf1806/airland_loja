66e44b9f52915225c4e0b299ad27f75944066439
Matheusmaf1806 <matheus.alvarenga@airland.com.br>
2025-04-16 11:42:09 -0300

    Update subdomain.js

--- middleware/subdomain.js
+++ middleware/subdomain.js
@@ -1,13 +1,13 @@
-// middleware/subdomain.js
 module.exports = (req, res, next) => {
-  const host = req.headers.host; // Exemplo: "lucastur.airland.com.br"
-  const mainDomain = 'airland.com.br';
+  const host = req.headers.host; // ex.: "maf.airland.com.br"
+  const mainDomain = "airland.com.br";
+  
   if (host && host.endsWith(mainDomain)) {
-    // Separa a parte que antecede o mainDomain (subdomínio)
-    const parts = host.split('.' + mainDomain);
-    req.subdomain = parts[0];
+    const sub = host.slice(0, host.length - mainDomain.length - 1); // Remove ".airland.com.br"
+    req.subdomain = sub || null;
   } else {
     req.subdomain = null;
   }
+
   next();
 };
